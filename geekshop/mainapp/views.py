6ef7600a9884ca33650ef728ddcc578fcd352862
dr2moscow <dr2moscow@gmail.com>
2021-07-19 22:31:56 +0300

    Выполнен пункт "5. *Организовать загрузку динамического контента в контроллеры с жесткого диска (например, в формате «json»)."
    Подгружаю меню на странице "Продукты" из JSON

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,17 +1,20 @@
-
+import json
 from django.shortcuts import render
 
 
 def index(request):
     title = 'Каталог'
 
-    links_menu = [
-        {'href': 'index', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    with open('mainapp/templates/mainapp/json/categories.json', encoding='utf-8') as json_file:
+        links_menu = json.load(json_file)
+
+    # links_menu = [
+    #     {'href': 'index', 'name': 'все'},
+    #     {'href': 'products_home', 'name': 'дом'},
+    #     {'href': 'products_office', 'name': 'офис'},
+    #     {'href': 'products_modern', 'name': 'модерн'},
+    #     {'href': 'products_classic', 'name': 'классика'},
+    # ]
 
     context = {
         'title': title,
