c7124e84f25956aedf0e35c98d92c2274cb46068
dr2moscow <dr2moscow@gmail.com>
2021-08-08 19:22:21 +0300

    ДЗ 7

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'mainapp',
     'authapp',
     'basketapp',
+    'adminapp',
 ]
 
 AUTH_USER_MODEL = 'authapp.ShopUser'
@@ -118,6 +119,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "geekshop", "static"),
 )
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
